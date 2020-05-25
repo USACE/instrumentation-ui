a5524aa38e906837d3ce85ac0b45f2116c1fad51
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2020-05-25 09:53:15 -0500

    handle some formatting and add images for the home page

--- src/app-components/hero.js
+++ src/app-components/hero.js
@@ -3,10 +3,28 @@ import { connect } from "redux-bundler-react";
 import Navbar from "./navbar";
 // import UploadButton from "./upload-button";
 
+import bg1 from "../img/bg-1.jpg";
+import bg2 from "../img/bg-2.jpg";
+import bg3 from "../img/bg-3.jpg";
+import bg4 from "../img/bg-4.jpg";
+import bg5 from "../img/bg-5.jpg";
+import bg6 from "../img/bg-6.jpg";
+import bg7 from "../img/bg-7.jpg";
+
+// const images = [bg1, bg2, bg3, bg4, bg5, bg6, bg7];
+// const i = Math.floor(Math.random() * 7);
+
 function App({ authIsLoggedIn }) {
   return (
     <div className="App">
-      <section className="hero is-primary">
+      <section
+        className="hero is-primary"
+        style={{
+          backgroundImage: `url(${bg1})`,
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+        }}
+      >
         <div className="hero-head">
           <Navbar theme="primary" hideBrand={true} />
         </div>
@@ -15,9 +33,11 @@ function App({ authIsLoggedIn }) {
             <h1 className="title">
               <i className="mdi mdi-water pr-2"></i>Instrumentation Browser
             </h1>
-            <h2 className="subtitle">Water data visualization</h2>
+            <h2 className="subtitle">Tools for visulizing water data</h2>
             <a href="/explore">
-              <button className="button is-warning">Explore the data</button>
+              <button className="is-hidden button is-warning">
+                Explore the data
+              </button>
             </a>
           </div>
         </div>
