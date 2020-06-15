d53cf987e0f408a02fe278a80760fe3e71d804e5
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2020-06-15 11:28:01 -0500

    format changes

--- src/app-bundles/time-series-measurements-bundle.js
+++ src/app-bundles/time-series-measurements-bundle.js
@@ -1,5 +1,5 @@
 import createRestBundle from "./create-rest-bundle";
-import { createSelector } from "redux-bundler"
+import { createSelector } from "redux-bundler";
 
 export default createRestBundle({
   name: "timeseriesMeasurements",
@@ -8,8 +8,7 @@ export default createRestBundle({
   staleAfter: 10000,
   persist: false,
   routeParam: "",
-  // getTemplate: `/timeseries/:timeseriesId/measurements?after=:after&before=:before`,
-  getTemplate: `/timeseries/:timeseriesId/measurements?after=1900-01-01T00:00:00.00Z&before=2021-01-01T00:00:00.00Z`,
+  getTemplate: `/timeseries/:timeseriesId/measurements?after=1900-01-01T00:00:00.00Z&before=2025-01-01T00:00:00.00Z`,
   putTemplate: "",
   postTemplate: "",
   deleteTemplate: "",
@@ -20,23 +19,25 @@ export default createRestBundle({
   addons: {
     selectTimeseriesMeasurementsX: createSelector(
       "selectTimeseriesMeasurementsItemsObject",
-      item => {
-        let time = []
+      (item) => {
+        let time = [];
         Object.keys(item).forEach(function (key) {
           let val = item[key];
-          val.items.map(x => time.push(x.time))
+          val.items.map((x) => time.push(x.time));
         });
         return time;
-      }),
+      }
+    ),
     selectTimeseriesMeasurementsY: createSelector(
       "selectTimeseriesMeasurementsItemsObject",
-      item => {
-        let value = []
+      (item) => {
+        let value = [];
         Object.keys(item).forEach(function (key) {
           let val = item[key];
-          val.items.map(y => value.push(y.value))
+          val.items.map((y) => value.push(y.value));
         });
-        return value
-      })
+        return value;
+      }
+    ),
   },
 });
