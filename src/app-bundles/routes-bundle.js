e9213598651d36b198dc5067d73d7bb99b660c2c
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2020-09-09 09:08:50 -0500

    add signup route

--- src/app-bundles/routes-bundle.js
+++ src/app-bundles/routes-bundle.js
@@ -3,11 +3,12 @@ import Home from "../app-pages/home/home";
 import Explorer from "../app-pages/explorer/explorer";
 import Instrument from "../app-pages/instrument/details";
 import Group from "../app-pages/group/details";
-import Help from "../app-pages/help/help"
+import Help from "../app-pages/help/help";
 import InstrumentManager from "../app-pages/manager/manager";
 import Uploader from "../app-pages/uploader/uploader";
 import Logout from "../app-pages/logout";
 import NotFound from "../app-pages/404";
+import SignUp from "../app-pages/signup/signup";
 
 export default createRouteBundle(
   {
@@ -15,6 +16,7 @@ export default createRouteBundle(
     "/": Home,
     "/help": Help,
     "/logout": Logout,
+    "/signup": SignUp,
     "/:projectSlug/upload": Uploader,
     "/:projectSlug/manager": InstrumentManager,
     "/:projectSlug/explore": Explorer,
