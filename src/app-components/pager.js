e009235be5d2fab95a4b2a3c80884aa6b3f852e9
KevinJJackson <kevinjjackson93@gmail.com>
2020-10-02 13:11:07 -0400

    refactor pagination

--- src/app-components/pager.js
+++ src/app-components/pager.js
@@ -10,7 +10,7 @@ const Page = ({ pageNo, isCurrent, onClick }) => {
   return (
     <li className={pageClass} onClick={onClick}>
       <a className="page-link" aria-label={`Goto page ${pageNo}`}>
-        {pageNo}
+        {pageNo + 1}
       </a>
     </li>
   );
@@ -70,53 +70,14 @@ export default ({ items, pageSize, children, itemsKey }) => {
               «
             </a>
           </li>
-
-          <Page
-            pageNo={1}
-            isCurrent={currentPage === 0}
-            onClick={() => {
-              setPage(0);
-            }}
-          />
-
-          {currentPage > 2 && pages.length > 5 ? (
-            <li className="page-item">
-              <span className="page-link">…</span>
-            </li>
-          ) : null}
-
-          {[1, 2, 3].map((_, i) => {
-            let p1 = currentPage - 1;
-            // if current page is 0, or 1 we start the count at idx = 1
-            if (currentPage <= 1) p1 = 1;
-            // if current page is greater than length - 2 we start the idx at length - 4
-            if (currentPage >= pages.length - 2) p1 = pages.length - 4;
-            return (
-              <Page
-                key={`${i}-${p1}`}
-                pageNo={p1 + i + 1}
-                isCurrent={currentPage === p1 + i}
-                onClick={() => {
-                  setPage(p1 + i);
-                }}
-              />
-            );
-          })}
-
-          {currentPage < pages.length - 2 && pages.length > 5 ? (
-            <li className="page-item">
-              <span className="page-link">…</span>
-            </li>
-          ) : null}
-
-          <Page
-            pageNo={pages.length}
-            isCurrent={currentPage === pages.length - 1}
-            onClick={() => {
-              setPage(pages.length - 1);
-            }}
-          />
-
+          {pages.map((_page, i) => (
+            <Page
+              key={`page-${i}`}
+              pageNo={i}
+              isCurrent={currentPage === i}
+              onClick={() => setPage(i)}
+            />
+          ))}
           <li className="page-item" onClick={pageUp}>
             <a className="page-link" aria-label={`Go to next page`}>
               »
