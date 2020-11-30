9823538bb2cf50874167cbad1e587effe57187a7
KevinJJackson <kevinjjackson93@gmail.com>
2020-11-30 10:13:37 -0500

    minor file changes

--- src/app-pages/home/project-card.js
+++ src/app-pages/home/project-card.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-export default ({ img, title, subtitle, href }) => {
+export default ({ project }) => {
+  const { img, title, subtitle, href } = project;
   const [isFavorite, setIsFavorite] = useState(false);
 
   const toggleFavorite = (e) => {
@@ -12,24 +13,24 @@ export default ({ img, title, subtitle, href }) => {
 
   return (
     <a href={href} tabIndex={0}>
-      <div className="card mb-5" style={{ maxWidth: "300px" }}>
-        <button className="favorite" onClick={toggleFavorite}>
+      <div className='card mb-5' style={{ maxWidth: '300px' }}>
+        <button className='favorite' onClick={toggleFavorite}>
           <span
             className={`mdi ${
-              isFavorite ? "mdi-star gold" : "mdi-star-outline"
+              isFavorite ? 'mdi-star gold' : 'mdi-star-outline'
             }`}
           />
         </button>
         {img ? (
           <img
-            style={{ height: "200px", width: "100%", display: "block" }}
+            style={{ height: '200px', width: '100%', display: 'block' }}
             src={img}
-            alt="Pretty scene associated with the project"
+            alt='Pretty scene associated with the project'
           />
         ) : null}
-        <div className="card-body">
-          <h5 className="card-title overflow-ellipsis">{title}</h5>
-          <h6 className="card-subtitle text-muted">{subtitle}</h6>
+        <div className='card-body'>
+          <h5 className='card-title overflow-ellipsis'>{title}</h5>
+          <h6 className='card-subtitle text-muted'>{subtitle}</h6>
         </div>
       </div>
     </a>
