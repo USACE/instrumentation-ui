3dfe97f501afd5e9504278f858713bb6aad7fe9f
KevinJJackson <kevinjjackson93@gmail.com>
2021-01-14 14:52:19 -0500

    update minify file

--- src/app-components/chart/minify-plotly.js
+++ src/app-components/chart/minify-plotly.js
@@ -1,9 +1,12 @@
 var Plotly = require('plotly.js/lib/core');
 
 Plotly.register([
-  require('plotly.js/lib/heatmap'),
-  require('plotly.js/lib/choropleth'),
-  require('plotly.js/lib/scattergeo')
+  /* 
+    Add any non-standard graph types needed in the application here, such as:
+    require('plotly.js/lib/heatmap'),
+
+    List of available imports can be found here `node_modules/plotly.js/lib/index.js`
+  */
 ]);
 
 module.exports = Plotly;
