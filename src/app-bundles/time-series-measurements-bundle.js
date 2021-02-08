876b06ff203b17189a0c8f8ef7f00939ca55e44c
KevinJJackson <kevinjjackson93@gmail.com>
2021-02-08 12:32:27 -0500

    fetch measurement data based on selected config, pull into chart component

--- src/app-bundles/time-series-measurements-bundle.js
+++ src/app-bundles/time-series-measurements-bundle.js
@@ -1,5 +1,8 @@
 import createRestBundle from './create-rest-bundle';
 
+const afterDate = '1900-01-01T00:00:00.00Z';
+const beforeDate = '2025-01-01T00:00:00.00Z';
+
 export default createRestBundle({
   name: 'timeseriesMeasurements',
   uid: 'timeseries_id',
@@ -7,7 +10,7 @@ export default createRestBundle({
   staleAfter: 10000,
   persist: false,
   routeParam: '',
-  getTemplate: '/timeseries/:timeseriesId/measurements?after=1900-01-01T00:00:00.00Z&before=2025-01-01T00:00:00.00Z',
+  getTemplate: `/timeseries/:timeseriesId/measurements?after=${afterDate}&before=${beforeDate}`,
   putTemplate: '',
   postTemplate: '/projects/:projectId/timeseries_measurements',
   deleteTemplate: '',
@@ -18,5 +21,46 @@ export default createRestBundle({
     'selectProjectsIdByRoute',
   ],
   mergeItems: true,
-  addons: {},
+  addons: {
+    doTimeseriesMeasurementsFetchById: ({
+      timeseriesId
+    }) => ({ dispatch, store, apiGet }) => {
+      dispatch({ type: 'TIMESERIES_FETCH_BY_ID_START' });
+
+      const url = `/timeseries/${timeseriesId}/measurements?after=${afterDate}&before=${beforeDate}`;
+      const flags = store['selectTimeseriesMeasurementsFlags']();
+      const itemsById = store['selectTimeseriesMeasurementsItemsObject']();
+      let fetchCount = store['selectTimeseriesMeasurementsFetchCount']();
+
+      apiGet(url, (_err, body) => {
+        new Array(body).forEach(item => itemsById[item['timeseries_id']] = item);
+
+        dispatch({
+          type: 'TIMSERIES_MEASUREMENTS_UPDATED_ITEM',
+          payload: {
+            ...itemsById,
+            ...flags,
+            ...{
+              _isLoading: false,
+              _isSaving: false,
+              _fetchCount: ++fetchCount,
+              _lastFetch: new Date(),
+              _lastResource: url,
+              _abortReason: null,
+            },
+          },
+        });
+        dispatch({ type: 'TIMESERIES_FETCH_BY_ID_START_FINISHED' });
+      });
+    },
+  },
+
+  reduceFurther: (state, { type, payload }) => {
+    switch (type) {
+      case 'TIMSERIES_MEASUREMENTS_UPDATED_ITEM':
+        return Object.assign({}, state, payload);
+      default:
+        return state;
+    }
+  },
 });
--- src/app-pages/reporting/batch-plot-chart.js
+++ src/app-pages/reporting/batch-plot-chart.js
@@ -1,23 +1,41 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { connect } from 'redux-bundler-react';
 
 import Chart from '../../app-components/chart/chart';
 import PlottingContext from './plotting-context';
 
 const BatchPlotChart = connect(
+  'doTimeseriesMeasurementsFetchById',
+  'doInstrumentTimeseriesSetActiveId',
+  'selectTimeseriesMeasurementsItems',
   'selectBatchPlotConfigurationsItems',
   ({
+    doTimeseriesMeasurementsFetchById,
+    doInstrumentTimeseriesSetActiveId,
+    timeseriesMeasurementsItems,
     batchPlotConfigurationsItems,
   }) => {
     const { selectedConfiguration } = useContext(PlottingContext);
+    const [timeseriesIds, setTimeseriesId] = useState([]);
+    const [measurements, setMeasurements] = useState([]);
 
     useEffect(() => {
-      console.log('selectedConfiguration: ', selectedConfiguration);
-      console.log('batchPlotConfigurationsItems: ', batchPlotConfigurationsItems);
+      const config = batchPlotConfigurationsItems.find(elem => elem.name === selectedConfiguration);
+      setTimeseriesId((config || {}).timeseries_id || []);
+    }, [selectedConfiguration, batchPlotConfigurationsItems, setTimeseriesId]);
 
-      const test = batchPlotConfigurationsItems.find(elem => elem.name === selectedConfiguration);
-      console.log('selected timeseries: ', (test || {}).timeseries_id);
-    }, [selectedConfiguration, batchPlotConfigurationsItems]);
+    useEffect(() => {
+      timeseriesIds.forEach(id => doTimeseriesMeasurementsFetchById({ timeseriesId: id }));
+    }, [timeseriesIds, doInstrumentTimeseriesSetActiveId]);
+
+    useEffect(() => {
+      const measurementItems = timeseriesIds.map(id => timeseriesMeasurementsItems.find(elem => elem.timeseries_id === id));
+      setMeasurements(measurementItems);
+    }, [timeseriesIds, timeseriesMeasurementsItems, setMeasurements]);
+
+    useEffect(() => {
+      console.log('current measurements: ', measurements);
+    }, [measurements]);
 
     return (
       <Chart />
