3ce8ca316ccb2638028925414156016a33232ae1
KevinJJackson <kevinjjackson93@gmail.com>
2021-02-16 10:37:06 -0500

    clean up map resizing

--- src/app-components/classMap.js
+++ src/app-components/classMap.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useLayoutEffect, useRef } from 'react';
-import { connect } from 'redux-bundler-react';
+import React, { useEffect, useLayoutEffect, useRef, forwardRef, useImperativeHandle } from 'react';
 import { fromLonLat } from 'ol/proj';
 import debounce from 'lodash.debounce';
 
-const Map = connect(
-  'doMapsInitialize',
-  'doMapsShutdown',
-  'selectMapsObject',
-  ({
-    doMapsInitialize,
-    doMapsShutdown,
-    mapsObject,
-    mapKey,
-    options,
-  }) => {
-    const el = useRef(null);
+const Map = forwardRef(({
+  doMapsInitialize,
+  doMapsShutdown,
+  mapsObject,
+  mapKey,
+  options,
+}, ref) => {
+  const el = useRef(null);
 
-    const updateSize = debounce(() => {
+  const updateSize = debounce(() => {
+    if (mapsObject[mapKey]) mapsObject[mapKey].updateSize();
+  }, 200);
+
+  const ro = new ResizeObserver(updateSize);
+
+  useImperativeHandle(ref, () => ({
+    updateSize: () => {
       if (mapsObject[mapKey]) mapsObject[mapKey].updateSize();
-    }, 200);
+    }
+  }));
 
-    const ro = new ResizeObserver(updateSize);
+  useLayoutEffect(() => {
+    let newOptions = options;
+    if (options && options.center) {
+      newOptions = {
+        ...options,
+        center: fromLonLat(options.center)
+      };
+    }
+    
+    doMapsInitialize(mapKey, el.current, newOptions);
+    ro.observe(el.current);
+  }, []);
 
-    useLayoutEffect(() => {
-      let newOptions = options;
-      if (options && options.center) {
-        newOptions = {
-          ...options,
-          center: fromLonLat(options.center)
-        };
-      }
-      
-      doMapsInitialize(mapKey, el.current, newOptions);
-      ro.observe(el.current);
-    }, []);
+  useEffect(() => () => doMapsShutdown(mapKey), [doMapsShutdown, mapKey]);
 
-    useEffect(() => () => doMapsShutdown(mapKey), [doMapsShutdown, mapKey]);
-
-    return (
-      <div
-        style={{ position: 'absolute', top: 0, right: 0, left: 0, bottom: 0 }}
-        ref={el}
-      />
-    );
-  }
-);
+  return (
+    <div
+      style={{ position: 'absolute', top: 0, right: 0, left: 0, bottom: 0 }}
+      ref={el}
+    />
+  );
+});
 
 export default Map;
--- src/app-pages/explorer/explorer.js
+++ src/app-pages/explorer/explorer.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { connect } from 'redux-bundler-react';
 import Panel from './panel';
 import Navbar from '../../app-components/navbar';
@@ -8,46 +8,63 @@ import Visualizations from './explorer-visualizations';
 import PanelGroup from 'react-panelgroup';
 import useWindowListener from '../../customHooks/useWindowListener';
 
-export default connect('selectExploreMapKey', ({ exploreMapKey: mapKey }) => {
-  const [landscapeMode, setLandscapeMode] = useState(false);
+export default connect(
+  'selectExploreMapKey',
+  'doMapsInitialize',
+  'doMapsShutdown',
+  'selectMapsObject',
+  ({
+    exploreMapKey: mapKey,
+    doMapsInitialize,
+    doMapsShutdown,
+    mapsObject,
+  }) => {
+    const [landscapeMode, setLandscapeMode] = useState(false);
+    const mapRef = useRef();
 
-  const toggleLandscape = useCallback(e => {
-    if (e.keyCode === 86 && e.shiftKey) {
-      setLandscapeMode(!landscapeMode);
-    }
-  }, [setLandscapeMode, landscapeMode]);
+    const toggleLandscape = useCallback(e => {
+      if (e.keyCode === 86 && e.shiftKey) {
+        setLandscapeMode(!landscapeMode);
+      }
+    }, [setLandscapeMode, landscapeMode]);
 
-  useWindowListener('keydown', toggleLandscape);
+    useWindowListener('keydown', toggleLandscape);
 
-  return (
-    <>
-      <Navbar theme='primary' />
-      <div
-        style={{
-          position: 'absolute',
-          top: 71,
-          right: 0,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <PanelGroup
-          borderColor='#ccc'
-          spacing={2}
-          direction={landscapeMode ? 'column' : 'row'}
+    return (
+      <>
+        <Navbar theme='primary' />
+        <div
+          style={{
+            position: 'absolute',
+            top: 71,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}
         >
-          <Panel>
-            <Map
-              mapKey={mapKey}
-              options={{ center: [-80.8027, 26.9419], zoom: 10 }}
-            />
-            <MapTools />
-          </Panel>
-          <Panel>
-            <Visualizations />
-          </Panel>
-        </PanelGroup>
-      </div>
-    </>
-  );
-});
+          <PanelGroup
+            borderColor='#ccc'
+            spacing={2}
+            direction={landscapeMode ? 'column' : 'row'}
+            onUpdate={(_data) => mapRef && mapRef.current && mapRef.current.updateSize()}
+          >
+            <Panel>
+              <Map
+                ref={mapRef}
+                mapKey={mapKey}
+                options={{ center: [-80.8027, 26.9419], zoom: 10 }}
+                doMapsInitialize={doMapsInitialize}
+                doMapsShutdown={doMapsShutdown}
+                mapsObject={mapsObject}
+              />
+              <MapTools />
+            </Panel>
+            <Panel>
+              <Visualizations />
+            </Panel>
+          </PanelGroup>
+        </div>
+      </>
+    );
+  }
+);
