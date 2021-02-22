ebf20441772681ac85a83c903ad556cb210c1054
KevinJJackson <kevinjjackson93@gmail.com>
2021-02-22 15:45:43 -0500

    filtering list working

--- src/app-components/multi-select/multi-select.js
+++ src/app-components/multi-select/multi-select.js
@@ -3,9 +3,15 @@ import React, { useEffect, useState, useRef } from 'react';
 import Dropdown from '../dropdown';
 import { DropdownButton, FilterInput } from './subcomponents';
 import { generateOption, reduceSelections } from './helper';
+import usePrevious from '../../customHooks/usePrevious';
+
+const getDisplay = elem => {
+  const { value, text } = elem;
+  return text ? text : value;
+};
 
 const MultiSelect = ({
-  text = 'Select Options',
+  text = 'Select Options ',
   className = '',
   menuClasses = '',
   onChange = () => {},
@@ -17,6 +23,9 @@ const MultiSelect = ({
   const [currentSelections, setCurrentSelections] = useState(initialValues);
   const [isAllSelected, setIsAllSelected] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [filteredOptions, setFilteredOptions] = useState(options);
+  const [inputValue, setInputValue] = useState('');
+  const previuosInputValue = usePrevious(inputValue);
   const dropdownRef = useRef();
   const inputRef = useRef();
 
@@ -29,14 +38,12 @@ const MultiSelect = ({
   };
 
   const handleOpen = () => {
-    console.log('opening!');
     if (dropdownRef && dropdownRef.current) {
       if (!isDropdownOpen) dropdownRef.current.openDropdown();
     }
   };
 
   const handleClose = () => {
-    console.log('closing!');
     if (dropdownRef && dropdownRef.current) {
       if (isDropdownOpen) dropdownRef.current.closeDropdown();
     }
@@ -53,6 +60,20 @@ const MultiSelect = ({
     }
   }, [onChange, currentSelections, withSelectAllOption, setIsAllSelected]);
 
+  useEffect(() => {
+    if (isFilterable) {
+      if (previuosInputValue !== inputValue) {
+        const newSet = options.filter(elem => {
+          const val = getDisplay(elem);
+  
+          return (String(val).toLowerCase()).indexOf(inputValue.toLowerCase()) !== -1;
+        });
+  
+        setFilteredOptions(newSet);
+      }
+    }
+  }, [isFilterable, previuosInputValue, inputValue]);
+
   return (
     <Dropdown.Menu
       ref={dropdownRef}
@@ -64,7 +85,7 @@ const MultiSelect = ({
             isHidden={isFilterable && isDropdownOpen}
           />
           <FilterInput
-            onChange={(val) => console.log('input changed! ', val)}
+            onChange={val => setInputValue(val)}
             handleClose={handleClose}
             isHidden={!isFilterable || (isFilterable && !isDropdownOpen)}
             ref={inputRef}
@@ -80,15 +101,23 @@ const MultiSelect = ({
         onClick: () => {},
       }}
     >
-      {withSelectAllOption && (
-        generateOption({ text: 'Select All' }, handleSelectAll, isAllSelected, 'all')
-      )}
-      {options.map((option, i) => {
-        const optionIsSelected = currentSelections.find(elem => elem === option.value);
-        const handleClick = () => setCurrentSelections(reduceSelections(option.value, currentSelections));
+      {filteredOptions.length ? (
+        <>
+          {withSelectAllOption && (
+            generateOption({ text: 'Select All' }, handleSelectAll, isAllSelected, 'all')
+          )}
+          {filteredOptions.map((option, i) => {
+            const optionIsSelected = currentSelections.find(elem => elem === option.value);
+            const handleClick = () => setCurrentSelections(reduceSelections(option.value, currentSelections));
 
-        return generateOption(option, handleClick, optionIsSelected, i);
-      })}
+            return generateOption(option, handleClick, optionIsSelected, i);
+          })}
+        </>
+      ) : (
+        <p className='text-dark mx-3 my-1'>
+          {isFilterable ? 'No Items Match Your Search' : 'No Options Provided'}
+        </p>
+      )}
     </Dropdown.Menu>
   );
 };
