9a00ff7cbe667e0959c48f75e5f9590805c1a878
thill02 <tessahill13@gmail.com>
2021-02-23 11:17:00 -0500

    fixed loading issue of legend

--- src/app-pages/explorer/map-legend.js
+++ src/app-pages/explorer/map-legend.js
@@ -1,40 +1,35 @@
 import React from 'react';
-import { connect } from 'redux-bundler-react';
-const status = {
-  'abandoned': 4,
-  'active': 1,
-  'destroyed': 5,
-  'inactive': 3,
-  'lost': 2
-};
-export default connect(
-  'selectDomainsItemsByGroup',
-  ({ domainsItemsByGroup: domains }) => {
-    domains.status.forEach((x) => {
-      x.title = x.value[0].toUpperCase() + x.value.substr(1).toLowerCase();
-      x.order = status[x.value];
-    });
-    domains.status.sort((a, b) => a.order > b.order ? 1 : -1);
-    return (
-      <div style={{ position: 'absolute', top: 10, right: 10 }}>
-        <div className='card' style={{ opacity: 0.8 }}>
-          <div className='card-body'>
-            {domains.status.map((x, i) => (
-              <p className='mb-2 mt-2' key={i}>
-                <svg width='15' height='15' className='mr-2'>
-                  <circle
-                    className={`legend-icon ${x.value}`}
-                    cx='8'
-                    cy='8'
-                    r='5'
-                  />
-                </svg>
-                {x.title}
-              </p>
-            ))}
-          </div>
+
+const status = [
+  { title: 'Abandoned', value: 'abandoned', order: 4 },
+  { title: 'Active', value: 'active', order: 1 },
+  { title: 'Destroyed', value: 'destroyed', order: 5 },
+  { title: 'Inactive', value: 'inactive', order: 3 },
+  { title: 'Lost', value: 'lost', order: 2 }
+];
+const MapLegend = () => {
+  status.sort((a, b) => a.order > b.order ? 1 : -1);
+  return (
+    <div style={{ position: 'absolute', top: 10, right: 10 }}>
+      <div className='card' style={{ opacity: 0.8 }}>
+        <div className='card-body'>
+          {status.map((x, i) => (
+            <p className='mb-2 mt-2' key={i}>
+              <svg width='15' height='15' className='mr-2'>
+                <circle
+                  className={`legend-icon ${x.value}`}
+                  cx='8'
+                  cy='8'
+                  r='5'
+                />
+              </svg>
+              {x.title}
+            </p>
+          ))}
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
+
+export default MapLegend;
