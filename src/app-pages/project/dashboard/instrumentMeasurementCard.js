ccff48d3241c4997cde4ec233aabc4be48e2fc7c
KevinJJackson <kevinjjackson93@gmail.com>
2021-04-22 12:49:22 -0400

    prepare instrument group rows for display

--- src/app-pages/project/dashboard/instrumentMeasurementCard.js
+++ src/app-pages/project/dashboard/instrumentMeasurementCard.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import { connect } from 'redux-bundler-react';
 
 import Card from '../../../app-components/card';
 import ProgressBar from '../../../app-components/progress-bar';
 
-const InstrumentMeasurementCard = () => (
-  <Card>
-    <Card.Header text='Instrument Groups' />
-    <Card.Body>
-      <p className='text-primary table-heading'>Active Measurements Today</p>
-      <div className='measurement-row'>
-        <a href='#'>Mohawk Dam</a>
-        <ProgressBar percent={52} />
-        <p className='measurement'>52</p>
-      </div>
-    </Card.Body>
-  </Card>
+const MeasurementRow = ({ name, url }) => (
+  <div className='measurement-row' key={name}>
+    <a href={url}>{name}</a>
+    <ProgressBar percent={52} />
+    <p className='measurement'>52</p>
+  </div>
+);
+
+const InstrumentMeasurementCard = connect(
+  'selectProjectsByRoute',
+  'selectInstrumentGroupsItems',
+  ({
+    projectsByRoute: project,
+    instrumentGroupsItems: groups,
+  }) => (
+    <Card>
+      <Card.Header text='Instrument Groups' />
+      <Card.Body>
+        <p className='text-primary table-heading'>Active Measurements Today</p>
+        {groups ? groups.map(group => {
+          const { name, slug } = group;
+          return (
+            <MeasurementRow
+              name={name}
+              url={`/${project.slug}/groups/${slug}`}
+            />
+          );
+        }) : <p>No Instrument Groups</p>}
+      </Card.Body>
+    </Card>
+  )
 );
 
 export default InstrumentMeasurementCard;
