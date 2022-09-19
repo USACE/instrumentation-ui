29372244c723736b1eb7d2c4de5e1ec0b01dfbc2
Kevin Jackson <Jackson_Kevin2@bah.com>
2022-09-19 15:25:24 -0400

    fix build errors

--- src/app-pages/reporting/batch-plotting/batch-plot-chart-settings.js
+++ src/app-pages/reporting/batch-plotting/batch-plot-chart-settings.js
@@ -1,24 +1,19 @@
 // @TODO: Work In Progress
 
-import React, { useEffect, useState } from 'react';
-import { subDays, startOfDay, endOfDay, isEqual, setDate } from 'date-fns';
-import { parseISO, format } from 'date-fns';
+import React from 'react';
+import { subDays, startOfDay } from 'date-fns';
 
 import PrintButton from './print-button';
 import Button from '../../../app-components/button';
-import ToggleButton from '../../../app-components/toggle-button';
 import DatePicker from 'react-datepicker';
-import Notifications from '../../../app-components/notifications';
 
 const dateAgo = (days) => subDays(new Date(), days);
 
 const BatchPlotChartSettings = ({
   dateRange = [],
   setDateRange,
-  setWithPrecipitation,
   lifetimeDate,
 }) => {
-
   const [fromTime, endTime] = dateRange;
 
   const alterRange = (daysAgo) => {
@@ -32,29 +27,7 @@ const BatchPlotChartSettings = ({
   return (
     <div className='m-2'>
       <PrintButton />
-      {/*<ToggleButton handleChange={val => setWithPrecipitation(val)} />*/}
       <div className='date-picker-group'>
-        {/*<Button
-          isOutline
-          text='30 Day'
-          variant='info'
-          isActive={isEqual(from, startOfDay(dateAgo(30))) && isEqual(to, endOfDay(new Date()))}
-          handleClick={() => setDateRange([startOfDay(dateAgo(30)), endOfDay(new Date())])}
-        />
-        <Button
-          isOutline
-          text='60 Day'
-          variant='info'
-          isActive={isEqual(from, startOfDay(dateAgo(60))) && isEqual(to, endOfDay(new Date()))}
-          handleClick={() => setDateRange([startOfDay(dateAgo(60)), endOfDay(new Date())])}
-        />
-        <Button
-          isOutline
-          text='90 Day'
-          variant='info'
-          isActive={isEqual(from, startOfDay(dateAgo(90))) && isEqual(to, endOfDay(new Date()))}
-          handleClick={() => setDateRange([startOfDay(dateAgo(90)), endOfDay(new Date())])}
-        />*/}
         <DatePicker
           selectsRange={true}
           startDate={fromTime}
@@ -90,7 +63,6 @@ const BatchPlotChartSettings = ({
           variant = 'info'
           handleClick={() => alterRange(30)}
         />
-        <Notifications />
       </div> 
     </div>
   );
