c462213196e1d0ecc306d714c1373ee64b49a550
Kevin Jackson <Jackson_Kevin2@bah.com>
2023-02-14 13:06:22 -0500

    custom sort instrument and timeseries

--- src/app-pages/project/data-loggers/tables/dataLoggerMappingTable.js
+++ src/app-pages/project/data-loggers/tables/dataLoggerMappingTable.js
@@ -6,6 +6,26 @@ import Card from '../../../../app-components/card';
 import Icon from '../../../../app-components/icon';
 import Table from '../../../../app-components/table';
 
+const instrumentSort = (rowA, rowB, instruments) => {
+  const { instrument_id: instrumentIdA } = rowA?.original;
+  const { instrument_id: instrumentIdB } = rowB?.original;
+
+  const instrumentAName = instruments.find(el => el.id === instrumentIdA)?.name;
+  const instrumentBName = instruments.find(el => el.id === instrumentIdB)?.name;
+
+  return instrumentAName?.localeCompare(instrumentBName);
+};
+
+const timeseriesSort = (rowA, rowB, timeseries) => {
+  const { timeseries_id: timeseriesIdA } = rowA?.original;
+  const { timeseries_id: timeseriesIdB } = rowB?.original;
+
+  const timeseriesAName = timeseries[timeseriesIdA]?.name;
+  const timeseriesBName = timeseries[timeseriesIdB]?.name;
+
+  return timeseriesAName?.localeCompare(timeseriesBName);
+};
+
 const DataLoggerMappingTable = connect(
   'selectInstrumentsItems',
   'selectInstrumentTimeseriesItemsObject',
@@ -45,14 +65,16 @@ const DataLoggerMappingTable = connect(
               key: 'instrument_id',
               header: 'Instrument',
               isSortable: true,
+              sortingFn: (rowA, rowB) => instrumentSort(rowA, rowB, instruments),
               render: (data) => {
                 const i = instruments?.find(el => el.id === data.instrument_id);
                 return <span>{i?.name}</span>;
-              }
+              },
             }, {
               key: 'timeseries_id',
               header: 'Timeseries',
               isSortable: true,
+              sortingFn: (rowA, rowB) => timeseriesSort(rowA, rowB, timeseries),
               render: (data) => {
                 const t = timeseries[data.timeseries_id];
                 return <span>{t?.name}</span>;
